68b8846d35cff7fd971e4ba257b2f5f37c37b8f5
Eskendarov <envereskendarov@gmail.com>
2020-11-12 16:20:40 +0300

    6. Перевернуть связанный список[161#389183]

---
+++ chapter_001/src/main/java/ru/job4j/collection/FlipLinkedList.java
@@ -0,0 +1,71 @@
+package ru.job4j.collection;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class FlipLinkedList<T> implements Iterable<T> {
+
+    private Node<T> head;
+
+    public void add(T value) {
+        final Node<T> node = new Node<>(value, null);
+        if (head == null) {
+            head = node;
+            return;
+        }
+        Node<T> tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
+        }
+        tail.next = node;
+    }
+
+    public void revert() {
+        if (head == null || head.next == null) {
+            return;
+        }
+        Node<T> previous = null;
+        Node<T> current = head;
+        Node<T> next;
+        while (current != null) {
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+        head = previous;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<>() {
+            Node<T> node = head;
+
+            @Override
+            public boolean hasNext() {
+                return node != null;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                final T value = node.value;
+                node = node.next;
+                return value;
+            }
+        };
+    }
+
+    private static class Node<T> {
+
+        final T value;
+        Node<T> next;
+
+        public Node(T value, Node<T> next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/collection/FlipLinkedListTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class FlipLinkedListTest {
+
+    @Test
+    public void whenAddThenIt() {
+        final FlipLinkedList<Integer> linked = new FlipLinkedList<>();
+        linked.add(1);
+        linked.add(2);
+        final Iterator<Integer> it = linked.iterator();
+        assertThat(it.next(), is(1));
+        assertThat(it.next(), is(2));
+    }
+
+    @Test
+    public void whenAddAndRevertThenIt() {
+        final FlipLinkedList<Integer> linked = new FlipLinkedList<>();
+        linked.add(1);
+        linked.add(2);
+        linked.revert();
+        final Iterator<Integer> it = linked.iterator();
+        assertThat(it.next(), is(2));
+        assertThat(it.next(), is(1));
+    }
+}
