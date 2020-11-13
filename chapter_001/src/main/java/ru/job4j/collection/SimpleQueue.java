f1a29bc9d0e259810fa47409abc1085fca599cb0
Eskendarov <envereskendarov@gmail.com>
2020-11-13 23:06:14 +0300

    5. Очередь на двух стеках [#389182] Исправил алгоритм.

--- chapter_001/src/main/java/ru/job4j/collection/SimpleQueue.java
+++ chapter_001/src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -1,25 +1,17 @@
 package ru.job4j.collection;
 
-import java.util.NoSuchElementException;
-
 public class SimpleQueue<T> {
 
     private final SimpleStack<T> in = new SimpleStack<>();
     private final SimpleStack<T> out = new SimpleStack<>();
 
     public T poll() {
-        if (!in.isEmpty()) {
-            do {
+        if (out.isEmpty()) {
+            while (!in.isEmpty()) {
                 out.push(in.pop());
-            } while (!in.isEmpty());
-            final T first = out.pop();
-            while (!out.isEmpty()) {
-                in.push(out.pop());
             }
-            return first;
-        } else {
-            throw new NoSuchElementException();
         }
+        return out.pop();
     }
 
     public void push(T value) {
