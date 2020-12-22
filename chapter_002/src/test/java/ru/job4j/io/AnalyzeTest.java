2f09bb9d61a741e1def4b51e5709455ad60638a5
Eskendarov <envereskendarov@gmail.com>
2020-12-22 12:47:48 +0300

    3.0. Тестирование IO [#389226]

--- chapter_002/src/test/java/ru/job4j/io/AnalyzeTest.java
+++ chapter_002/src/test/java/ru/job4j/io/AnalyzeTest.java
@@ -1,11 +1,11 @@
 package ru.job4j.io;
 
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,28 +14,30 @@ import static org.hamcrest.CoreMatchers.is;
 
 public class AnalyzeTest {
 
-    /**
-     * source.log:
-     * 200 10:56:01
-     * 500 10:57:01
-     * 400 10:58:01
-     * 200 10:59:01
-     * 300 11:00:03
-     * 500 11:01:02
-     * 200 11:02:02
-     */
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
     @Test
-    public void unavailableDiapasons() {
-        final String source = "./data/source.log";
-        final String target = "./data/target.log";
-        new Analyze().unavailable(source, target);
-        try (BufferedReader read = new BufferedReader(new FileReader(target))) {
-            final List<String> list = read.lines().collect(Collectors.toList());
-            Assert.assertThat(list, is(
-                    Arrays.asList("10:57:01;10:59:01;", "11:01:02;11:02:02;")
-            ));
-        } catch (IOException e) {
-            e.printStackTrace();
+    public void unavailableDiapasons() throws IOException {
+        final File source = folder.newFile("source.log");
+        final File target = folder.newFile("target.log");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("200 10:56:01");
+            out.println("500 10:57:01");
+            out.println("400 10:58:01");
+            out.println("200 10:59:01");
+            out.println("300 11:00:03");
+            out.println("500 11:01:02");
+            out.println("200 11:02:02");
         }
+        new Analyze().unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        final List<String> targetList;
+        try (BufferedReader read = new BufferedReader(new FileReader(target))) {
+            targetList = read.lines().collect(Collectors.toList());
+
+        }
+        Assert.assertThat(targetList, is(
+                Arrays.asList("10:57:01;10:59:01;", "11:01:02;11:02:02;")
+        ));
     }
 }
