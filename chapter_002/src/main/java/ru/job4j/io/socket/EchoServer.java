045e6244bc74be3fc190faf037f49ab073a2ccdb
Eskendarov <envereskendarov@gmail.com>
2021-01-17 05:16:06 +0300

    0. Что такое Socket? [#389250]

---
+++ chapter_002/src/main/java/ru/job4j/io/socket/EchoServer.java
@@ -0,0 +1,35 @@
+package ru.job4j.io.socket;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class EchoServer {
+
+    public static void main(String[] args) throws IOException {
+        try (ServerSocket server = new ServerSocket(9000)) {
+            final StringBuilder msg = new StringBuilder();
+            while (!(msg.toString().equals("bye"))) {
+                msg.setLength(0);
+                final Socket socket = server.accept();
+                try (OutputStream out = socket.getOutputStream();
+                     BufferedReader in = new BufferedReader(
+                             new InputStreamReader(socket.getInputStream()))
+                ) {
+                    in.lines().takeWhile(line -> !line.isEmpty())
+                            .forEach(line -> {
+                                System.out.println(line);
+                                if (line.contains("msg=")) {
+                                    msg.append(line, line.indexOf("=") + 1,
+                                            line.lastIndexOf(" "));
+                                }
+                            });
+                    out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
+                }
+            }
+        }
+    }
+}
