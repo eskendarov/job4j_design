5701a6b8a893b5591faa76acaef16db28610995c
Eskendarov <envereskendarov@gmail.com>
2021-01-22 07:14:17 +0300

    3. Slf4j - вывод переменных. [#389222]

--- chapter_002/src/main/java/ru/job4j/io/logger/UsageLog4j.java
+++ chapter_002/src/main/java/ru/job4j/io/logger/UsageLog4j.java
@@ -14,5 +14,17 @@ public class UsageLog4j {
         LOG.info("info message");
         LOG.warn("warn message");
         LOG.error("error message");
+        final byte bt = 12;
+        final short sh = 44;
+        final int in = 3452;
+        final long lng = 23535L;
+        final float fl = 0.45f;
+        final double db = 34.2d;
+        final boolean bln = true;
+        final char ch = 'A';
+        LOG.debug("byte: {}, short: {}, int: {}, long: {},"
+                        + "float: {},double: {}, boolean: {}, char: {}",
+                bt, sh, in, lng, fl, db, bln, ch
+        );
     }
 }
